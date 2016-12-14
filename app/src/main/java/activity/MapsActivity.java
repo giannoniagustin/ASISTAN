ceb9f110eb9bb2f7927aa56c2eb672db0e4d1cb4
Gianno <giannoniagustin@gmail.com>
2016-12-14 16:19:13 -0300

    Ajustes codigo

--- app/src/main/java/activity/MapsActivity.java
+++ app/src/main/java/activity/MapsActivity.java
@@ -22,13 +22,12 @@ import archivos.Log;
 import giannno.asistan.R;
 import google.ubicacion.CapaServicio;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
+{
     private GoogleMap mMap;
     private CapaServicio capaServicio = new CapaServicio(this);
     private BroadcastReceiver broadcastReceiver;
     private LatLng latLng = null;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         try {
@@ -40,15 +39,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                     .findFragmentById(R.id.map);
             mapFragment.getMapAsync(this);
-
-
-
         } catch (Exception e)
         {
             Log.LOGGER.severe(e.toString());
         }
     }
-
 
     /**
      * Manipulates the map once available.
@@ -60,25 +55,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap)
+    {
         mMap = googleMap;
         checkPermissionAndroid();
         // Add a marker in Sydney and move the camera
-
     }
-
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
-
-    public void checkPermissionAndroid(){
+    //El siguiente metodo realiza la aceptacion de permisos de la app .La cual cambio desde el API 23 y se hace individualmente
+    public void checkPermissionAndroid()
+    {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED  &&
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
         {
-
             ActivityCompat.requestPermissions (this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.ACCESS_COARSE_LOCATION,
                     Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -86,13 +81,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         }
 
-
-
-
     }
     //Metodo para suscribirse al pedido de ubicacion que se le solicita al servicio. Cuando se consiga
     //un resultado, el broadcast lo recibira
-    public void suscribirseUbicacion() {
+    public void suscribirseUbicacion()
+    {
         try {
             broadcastReceiver = new BroadcastReceiver() {
                 @Override
@@ -104,10 +97,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             };
             registerReceiver(broadcastReceiver, new IntentFilter("resultado"));
         }catch (Exception e)
-        {Log.LOGGER.severe(e.toString());}
+        {
+            Log.LOGGER.severe(e.toString());}
     }
     //Metodo para avisarle a la capa del servicio, que no queremos recibir mas la informacion de la ubicacion.
-    public void desuscribirseUbicacion(BroadcastReceiver b){
+    public void desuscribirseUbicacion(BroadcastReceiver b)
+    {
         try {
             unregisterReceiver(b);
             capaServicio.desvincularBroadcast(this);
