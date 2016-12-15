6ccfd908c21644c3b32b2dbb80c9ad67f4ee604b
Gianno <giannoniagustin@gmail.com>
2016-12-15 15:59:14 -0300

    Ajustes Archivos

--- app/src/main/java/archivos/ConjuntoArchivos.java
+++ app/src/main/java/archivos/ConjuntoArchivos.java
@@ -1,8 +1,40 @@
 package archivos;
 
+import java.util.Vector;
+
 /**
  * Created by Gianno on 14/12/2016.
  */
 
 public class ConjuntoArchivos extends ElmentoCarpeta {
+    Vector<ElmentoCarpeta> elmentoCarpetas;
+
+    public ConjuntoArchivos(Vector<ElmentoCarpeta> elmentoCarpetas) {
+        this.elmentoCarpetas = elmentoCarpetas;
+    }
+
+    public void agregarElementoCarpeta(ElmentoCarpeta eCarpeta)
+    {
+        elmentoCarpetas.add(eCarpeta);
+    }
+
+    public Vector<ElmentoCarpeta> getElmentoCarpetas()
+    {
+        return elmentoCarpetas;
+    }
+
+    public void setElmentoCarpetas(Vector<ElmentoCarpeta> elmentoCarpetas)
+    {
+        this.elmentoCarpetas = elmentoCarpetas;
+    }
+
+    @Override
+    public void comprimir()
+    {
+        for(int i=0;i < elmentoCarpetas.size();i++)
+        {
+            elmentoCarpetas.elementAt(i).comprimir();
+        }
+
+    }
 }
--- app/src/main/java/archivos/ElmentoCarpeta.java
+++ app/src/main/java/archivos/ElmentoCarpeta.java
@@ -9,4 +9,6 @@ public abstract class ElmentoCarpeta
     String nombre;
     String path;
     ManangerArchivos manangerArchivos;
+
+    public abstract void comprimir();
 }
